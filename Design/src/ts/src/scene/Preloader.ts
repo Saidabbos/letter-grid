b98d001804d9786bab15a7de7bda06a34c855ec7
par0m <romanparada100500@gmail.com>
2020-12-28 21:37:28 +0200

    arabic vowels update

--- Design/src/ts/src/scene/Preloader.ts
+++ Design/src/ts/src/scene/Preloader.ts
@@ -3,11 +3,16 @@ module sh.scene {
     export class Preloader extends Phaser.Scene {
 
         preload() {
+            this.load.json('gameplay', 'assets/json/gameplay.json');
         }
 
         create() {
-            this.load.json('gameplay', 'assets/json/gameplay.json');
             this.load.pack('preloader', 'assets/pack.json');
+
+            let json = game.cache.json.get('gameplay');
+            for (let l of json["letters"]) {
+                this.load.image(l["correctLetterName"], "assets/imgs/letters/"+l["correctLetterName"]+".png");
+            }
 
             this.load.on('progress', (value:number) => {
             }, this);
